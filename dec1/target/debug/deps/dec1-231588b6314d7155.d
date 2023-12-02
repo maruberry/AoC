6945f172e85190480aec2b075d2f303285a11485
maruberry <152601094+maruberry@users.noreply.github.com>
2023-12-02 18:33:23 +0200

    Delete dec1/target directory

--- dec1/target/debug/deps/dec1-231588b6314d7155.d
+++
@@ -1,5 +0,0 @@
-/home/maru/advent_code/dec1/dec1/target/debug/deps/dec1-231588b6314d7155: src/main.rs
-
-/home/maru/advent_code/dec1/dec1/target/debug/deps/dec1-231588b6314d7155.d: src/main.rs
-
-src/main.rs:
