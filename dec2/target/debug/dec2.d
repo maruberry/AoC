bdd0e1b9d307de5b50dfda5d7152dfcca4bd152a
maruberry <152601094+maruberry@users.noreply.github.com>
2023-12-02 18:33:41 +0200

    Delete dec2/target directory

--- dec2/target/debug/dec2.d
+++
@@ -1,1 +0,0 @@
-/home/maru/advent_code/dec2/target/debug/dec2: /home/maru/advent_code/dec2/src/main.rs
--- dec2/target/debug/deps/dec2-6fab9eb2734f3b7d.d
+++
@@ -1,5 +0,0 @@
-/home/maru/advent_code/dec2/target/debug/deps/dec2-6fab9eb2734f3b7d: src/main.rs
-
-/home/maru/advent_code/dec2/target/debug/deps/dec2-6fab9eb2734f3b7d.d: src/main.rs
-
-src/main.rs:
